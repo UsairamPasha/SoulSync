5a0162a07e5c1d548b3a57a772cbd0bce9b64be0
Muhammad Usairam Pasha <Shubanapasha@gmail.com>
2026-09-01 21:04:28 +0500

    Update active server URL to not-committees-pike-destinations

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -14,7 +14,7 @@ class AppConfig {
   const AppConfig({
     this.environment = AppEnvironment.dev,
     this.baseUrl =
-        'https://reel-releases-logging-bean.trycloudflare.com',
+        'https://not-committees-pike-destinations.trycloudflare.com',
     this.apiVersion = 'v1',
     this.connectTimeout = const Duration(seconds: 60),
     this.receiveTimeout = const Duration(seconds: 60),
@@ -48,7 +48,7 @@ class AppConfig {
 class ServerUrlNotifier extends StateNotifier<String> {
   final ServerUrlStorageService _storageService;
   static const String defaultUrl =
-      'https://reel-releases-logging-bean.trycloudflare.com';
+      'https://not-committees-pike-destinations.trycloudflare.com';
 
   ServerUrlNotifier(this._storageService) : super(defaultUrl) {
     _init();
