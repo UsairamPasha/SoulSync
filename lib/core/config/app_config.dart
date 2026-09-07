f9e1ec049d72cf6337045bc065427d61d7882dab
Muhammad Usairam Pasha <Shubanapasha@gmail.com>
2026-09-07 22:15:02 +0500

    Update default backend Cloudflare tunnel URL to adsl-calculator-overnight-livecam

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -14,7 +14,7 @@ class AppConfig {
   const AppConfig({
     this.environment = AppEnvironment.dev,
     this.baseUrl =
-        'https://sheriff-wages-loves-certificate.trycloudflare.com',
+        'https://adsl-calculator-overnight-livecam.trycloudflare.com',
     this.apiVersion = 'v1',
     this.connectTimeout = const Duration(seconds: 60),
     this.receiveTimeout = const Duration(seconds: 60),
@@ -48,7 +48,7 @@ class AppConfig {
 class ServerUrlNotifier extends StateNotifier<String> {
   final ServerUrlStorageService _storageService;
   static const String defaultUrl =
-      'https://sheriff-wages-loves-certificate.trycloudflare.com';
+      'https://adsl-calculator-overnight-livecam.trycloudflare.com';
 
   ServerUrlNotifier(this._storageService) : super(defaultUrl) {
     _init();
