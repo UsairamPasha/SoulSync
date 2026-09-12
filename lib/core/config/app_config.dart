cbd1510f621cdc894f643fe405867ff1d7dec3c5
Muhammad Usairam Pasha <Shubanapasha@gmail.com>
2026-09-12 11:30:49 +0500

    Update default backend Cloudflare tunnel URL to promoting-administered-converted-bunny

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -14,7 +14,7 @@ class AppConfig {
   const AppConfig({
     this.environment = AppEnvironment.dev,
     this.baseUrl =
-        'https://boat-ton-meanwhile-shipment.trycloudflare.com',
+        'https://promoting-administered-converted-bunny.trycloudflare.com',
     this.apiVersion = 'v1',
     this.connectTimeout = const Duration(seconds: 60),
     this.receiveTimeout = const Duration(seconds: 60),
@@ -48,7 +48,7 @@ class AppConfig {
 class ServerUrlNotifier extends StateNotifier<String> {
   final ServerUrlStorageService _storageService;
   static const String defaultUrl =
-      'https://boat-ton-meanwhile-shipment.trycloudflare.com';
+      'https://promoting-administered-converted-bunny.trycloudflare.com';
 
   ServerUrlNotifier(this._storageService) : super(defaultUrl) {
     _init();
