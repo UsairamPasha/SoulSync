fdd85dcb406df96a489928f0e5f72bac786835f8
Muhammad Usairam Pasha <Shubanapasha@gmail.com>
2026-09-18 19:10:29 +0500

    Update backend Cloudflare tunnel URL to documentary-referrals-easy-cds

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -14,7 +14,7 @@ class AppConfig {
   const AppConfig({
     this.environment = AppEnvironment.dev,
     this.baseUrl =
-        'https://rca-weblog-breakdown-experience.trycloudflare.com',
+        'https://documentary-referrals-easy-cds.trycloudflare.com',
     this.apiVersion = 'v1',
     this.connectTimeout = const Duration(seconds: 60),
     this.receiveTimeout = const Duration(seconds: 60),
@@ -48,7 +48,7 @@ class AppConfig {
 class ServerUrlNotifier extends StateNotifier<String> {
   final ServerUrlStorageService _storageService;
   static const String defaultUrl =
-      'https://rca-weblog-breakdown-experience.trycloudflare.com';
+      'https://documentary-referrals-easy-cds.trycloudflare.com';
 
   ServerUrlNotifier(this._storageService) : super(defaultUrl) {
     _init();
