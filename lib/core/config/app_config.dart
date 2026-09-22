f16a77d272538412aa10d370d0d54879bfbe6161
Muhammad Usairam Pasha <Shubanapasha@gmail.com>
2026-09-22 15:16:37 +0500

    Update backend Cloudflare tunnel URL to happening-relying-why-obituaries

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -14,7 +14,7 @@ class AppConfig {
   const AppConfig({
     this.environment = AppEnvironment.dev,
     this.baseUrl =
-        'https://wave-relation-apparel-propecia.trycloudflare.com',
+        'https://happening-relying-why-obituaries.trycloudflare.com',
     this.apiVersion = 'v1',
     this.connectTimeout = const Duration(seconds: 60),
     this.receiveTimeout = const Duration(seconds: 60),
@@ -48,7 +48,7 @@ class AppConfig {
 class ServerUrlNotifier extends StateNotifier<String> {
   final ServerUrlStorageService _storageService;
   static const String defaultUrl =
-      'https://wave-relation-apparel-propecia.trycloudflare.com';
+      'https://happening-relying-why-obituaries.trycloudflare.com';
 
   ServerUrlNotifier(this._storageService) : super(defaultUrl) {
     _init();
